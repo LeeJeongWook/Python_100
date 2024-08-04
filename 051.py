ca6d696eee7d2965e1d75792e3d1b0be612c97c4
leejw0709@gmail.com <leejw0709@gmail.com>
2024-08-04 18:43:55 +0900

    commit

---
+++ 051.py
@@ -0,0 +1,5 @@
+n = int(input("카운트다운 몇 초 전인가요?"))
+
+for i in range(n,0,-1):
+    print(i)
+print("발사!")
---
+++ 052.py
@@ -0,0 +1,5 @@
+n = int(input())
+result = int(0)
+for i in range(n+1):
+    result += i
+print("1부터", n, "까지 모두 더한 합은", result)
